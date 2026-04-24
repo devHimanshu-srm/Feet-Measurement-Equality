127de000bc08ff01df20dbf2469a42cad311a6c4
devHimanshu-srm <devhimanshu311@gmail.com>
2026-04-24 14:43:22 +0530

    UC4 - Added YARD and CM support

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -2,30 +2,34 @@ import java.util.Scanner;
 
 public class QuantityMeasurementApp {
 
-    // Method to compare two values in feet
-    public static boolean compareFeet(double value1, double value2) {
-        return Double.compare(value1, value2) == 0;
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.print("Enter first value in feet: ");
-            double feet1 = scanner.nextDouble();
+            // Input 1
+            System.out.print("Enter value 1: ");
+            double value1 = scanner.nextDouble();
+            System.out.print("Enter unit 1 (FEET/INCH/YARD/CM): ");
+            Unit unit1 = Unit.valueOf(scanner.next().toUpperCase());
 
-            System.out.print("Enter second value in feet: ");
-            double feet2 = scanner.nextDouble();
+            // Input 2
+            System.out.print("Enter value 2: ");
+            double value2 = scanner.nextDouble();
+            System.out.print("Enter unit 2 (FEET/INCH/YARD/CM): ");
+            Unit unit2 = Unit.valueOf(scanner.next().toUpperCase());
 
-            if (compareFeet(feet1, feet2)) {
+            Quantity q1 = new Quantity(value1, unit1);
+            Quantity q2 = new Quantity(value2, unit2);
+
+            if (q1.equals(q2)) {
                 System.out.println("Values are equal.");
             } else {
                 System.out.println("Values are NOT equal.");
             }
 
         } catch (Exception e) {
-            System.out.println("Invalid input! Please enter numeric values only.");
+            System.out.println("Invalid input! Use FEET, INCH, YARD, or CM.");
         } finally {
             scanner.close();
         }
